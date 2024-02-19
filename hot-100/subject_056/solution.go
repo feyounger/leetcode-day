3c279f6be96ed9f3aff273cb968564debb8287fb
feyoung <fyliucf@isoftstone.com>
2024-02-19 18:42:20 +0800

    feat(root): 添加 刷两道题

---
+++ hot-100/subject_056/solution.go
@@ -0,0 +1,26 @@
+package subject_056
+
+import "sort"
+
+func merge(intervals [][]int) [][]int {
+	sort.Slice(intervals, func(i, j int) bool {
+		return intervals[i][0] < intervals[j][0]
+	})
+	var res [][]int
+	res = append(res, intervals[0])
+	for i := 1; i < len(intervals); i++ {
+		if res[len(res)-1][1] < intervals[i][0] {
+			res = append(res, intervals[i])
+		} else {
+			res[len(res)-1][1] = max(res[len(res)-1][1], intervals[i][1])
+		}
+	}
+	return res
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
---
+++ hot-100/subject_056/solution_test.go
@@ -0,0 +1,10 @@
+package subject_056
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestMerge(t *testing.T) {
+	fmt.Println(merge([][]int{{1, 3}, {2, 6}, {8, 10}, {15, 18}}))
+}
