f763db4b6828ecbf6119bd6322d4acb0a71240d9
feyoung <fyliucf@isoftstone.com>
2024-02-19 16:23:05 +0800

    feat(root): 添加 刷两道题

---
+++ array_linked/subject_167/solution_test.go
@@ -0,0 +1,11 @@
+package subject_167
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestTwoSum(t *testing.T) {
+	arr := twoSum([]int{2, 7, 11, 15}, 9)
+	fmt.Println(arr)
+}
---
+++ array_linked/subject_344/solution_test.go
@@ -0,0 +1,7 @@
+package subject_344
+
+import "testing"
+
+func TestReverseString(t *testing.T) {
+	reverseString([]byte{1, 2, 3, 4})
+}
