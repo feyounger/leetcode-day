2a262502f4da82ce06170c600d6c3126a6c3676d
feyoung <fyliucf@isoftstone.com>
2024-02-19 14:33:53 +0800

    feat(root): 添加 刷两道题

--- hot-100/subject_283/solution.go
+++ hot-100/subject_283/solution.go
@@ -5,18 +5,15 @@ package subject_283
 283 移动零
 */
 func moveZeroes(nums []int) {
-	j := 0
-	for i := 0; i < len(nums) && j < len(nums); j++ {
-		if nums[i] == 0 {
-			if nums[j] != 0 {
-				nums[i] = nums[j]
-				nums[j] = 0
-				i++
-			} else {
-				continue
-			}
-		} else {
-			i++
+	slow, fast := 0, 0
+	for fast < len(nums) {
+		if nums[fast] != 0 {
+			nums[slow] = nums[fast]
+			slow++
 		}
+		fast++
+	}
+	for i := slow; i < len(nums); i++ {
+		nums[i] = 0
 	}
 }
