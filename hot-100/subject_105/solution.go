e70140a60894bd7bc93ad058c9a38f9447f0ecec
feyoung <1477865250@qq.com>
2024-11-18 10:56:02 +0800

    feat(hot100): 添加 从前序和中序构造二叉树

---
+++ hot-100/subject_105/solution.go
@@ -0,0 +1,24 @@
+package subject_105
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func buildTree(preorder []int, inorder []int) *TreeNode {
+	if len(preorder) == 0 {
+		return nil
+	}
+	root := &TreeNode{preorder[0], nil, nil}
+
+	m := make(map[int]int)
+	for i, v := range inorder {
+		m[v] = i
+	}
+	i := m[preorder[0]]
+
+	root.Left = buildTree(preorder[1:len(inorder[:i])+1], inorder[:i])
+	root.Right = buildTree(preorder[len(inorder[:i])+1:], inorder[i+1:])
+	return root
+}
